d654b02415f401e697f266c0e84cbf9119c0db7c
yjkang <yjkang@agos.co.kr>
2023-10-06 11:20:26 +0900

    231006 css 적용 시작

--- src/pages/Template.jsx
+++ src/pages/Template.jsx
@@ -1,13 +1,29 @@
 import { useState, useCallback } from "react";
 import { MdCheckBoxOutlineBlank, MdCheckBox, MdDelete, MdNumbers } from "react-icons/md";
 import cn from "classnames";
+import "bulma/css/bulma.css";
 import "components/Todo.scss";
 
 //기본 템플릿
 const TodoTemplate = ({ children }) => {
     return (
         <div className="TodoTemplate">
-            <div className="app-title"><h1>할일 목록</h1></div>
+            <div>
+                <nav>
+                    <ul>
+                        <li><button class="button is-light">Main</button></li>
+                        <li><button class="button is-light">Contact Us</button></li>
+                        <li><button class="button is-light">My Page</button></li>
+                        <li><button class="button is-light">More Info</button></li>
+                    </ul>
+                </nav>
+            </div>          
+            <div class="tags are-large" style={{"margin-top": "50px"}}>
+                <div class="tags has-addons">
+                    <span class="tag is-dark" style={{"width": "180px", "font-weight": "bold"}}>할일 목록</span>
+                    <span class="tag is-info">0.0.1 v</span>
+                </div>
+            </div>
             <div className="content">{children}</div>
         </div>
     );
@@ -31,8 +47,8 @@ const TodoAdd = ({ addTodo }) => {
 
     return (
         <form className="TodoAdd" onSubmit={onSubmit}>
-            <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
-            <button type="submit">등록하기</button>
+            <input className="input" placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
+            <button className="button is-info is-outlined" type="submit" style={{"font-weight": "bold"}}>등록하기</button>
         </form>
     )
 }
@@ -43,13 +59,13 @@ export {TodoAdd};
 //할일 목록
 const TodoList = ({ todos, deleteTodo, checkTodo }) => {
     return (
-        <div className="TodoList">
+        <div className="TodoList-Table-Head">
             <thead>
                 <tr>
                     <th className="todoIds" style={{"width": "50px"}}>No.</th>
                     <th className="titles" style={{"width": "300px", "textAlign": "center"}}>What's To do?</th>
                     <th className="checked" style={{"width": "100px", "textAlign":"center"}}>Select</th>
-                    <th className="delete" style={{"width": "100px", "textAlign":"center"}}>Delete</th>
+                    <th className="delete?" style={{"width": "100px", "textAlign":"center"}}>Delete</th>
                 </tr>
             </thead>
             {todos.map((todo) => (
@@ -67,13 +83,13 @@ export {TodoList};
 const TodoListItem = ({ todo, deleteTodo, checkTodo }) => {
     const { id, title, checked } = todo;
     return (
-        <div className="TodoList-Table">
+        <div className="TodoList-Table-Body">
             <tbody>
                 <tr>
                     <td className="todoId" style={{"width": "50px"}}><MdNumbers />{id}</td>
-                    <td className="title" style={{"width": "300px", "textAlign":"center"}}>{title}</td>
+                    <td className="todoTitle" style={{"width": "300px", "textAlign":"center"}}>{title}</td>
                     <td className={cn("checkbox", { checked })} onClick={() => checkTodo(id)} style={{"width": "100px", "textAlign":"center"}}>{checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}</td>
-                    <td className="delete" onClick={() => deleteTodo(id)} style={{"width": "100px", "textAlign":"center"}}><MdDelete /> </td>
+                    <td className="btn-delete" onClick={() => deleteTodo(id)} style={{"width": "100px", "textAlign":"center"}}><MdDelete /> </td>
                 </tr>
             </tbody>
         </div>
